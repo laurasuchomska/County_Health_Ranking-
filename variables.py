f4a074795bb9e9bd225ccb065897ff699c05c4b5
laurasuchomska <46826425+laurasuchomska@users.noreply.github.com>
2020-02-24 15:34:14 +0000

    cleaning data

--- variables.py
+++ variables.py
@@ -4,7 +4,6 @@ keywords = ["denominator",
             "Black",
             "White",
             "Hispanic",
-            "FIPS",
             "Alaskan",
             "Indian",
             "Hawaiian"]
